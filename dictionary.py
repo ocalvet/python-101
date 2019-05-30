662494fb7a3065c6dee7cf43269fef21af2a4439
Ovidio R. Calvet <calveto@gmail.com>
2019-05-30 15:17:10 -0400

    Loop through keys in a dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,7 @@
 sample_dic = {"name": "Some Name", "description": "Descriptio  of the name",
               "title": "The title", "amount": 12.4}
 
-print(sample_dic['name'])
+for key in sample_dic.keys():
+    print("%s --- %s" % (key, sample_dic[key]))
+
+print('The End')
