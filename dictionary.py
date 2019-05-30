a7bfa1eab22d371a0fc29ab5e01b03db22431ccd
Ovidio R. Calvet <calveto@gmail.com>
2019-05-30 15:10:40 -0400

    Add more keys to dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,4 @@
-sample_dic = {"name": "Some Name"}
+sample_dic = {"name": "Some Name", "description": "Descriptio  of the name",
+              "title": "The title", "amount": 12.4}
 
 print(sample_dic['name'])
