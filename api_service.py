95c70111807bea64460f471237c03e7753a5ecdf
Ovidio R. Calvet <calveto@gmail.com>
2019-06-04 19:43:23 -0400

    Add some documentation and a constructor

--- api_service.py
+++ api_service.py
@@ -1,3 +1,8 @@
 class ApiService(object):
+    """A service to make api calls"""
+
+    def __init__(self):
+        self.title = "Api service"
+
     def say(self):
-        return "Hello"
+        return "Hello %s" % self.title
